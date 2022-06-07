3000a5a96cbf79997a7cce3a52a83e46b305932f
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-06-07 16:25:35 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/whiz/springbootmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.whiz.springbootmongo.repository;
+
+import com.whiz.springbootmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/whiz/springbootmongo/service/UserService.java
@@ -0,0 +1,20 @@
+package com.whiz.springbootmongo.service;
+
+import com.whiz.springbootmongo.domain.User;
+import com.whiz.springbootmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
