f55a192c76be50e38a32a2b4ebe68c4d047cbb14
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-06-22 10:29:56 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/whiz/springbootmongo/service/UserService.java
+++ src/main/java/com/whiz/springbootmongo/service/UserService.java
@@ -5,6 +5,7 @@ import com.whiz.springbootmongo.dto.UserDTO;
 import com.whiz.springbootmongo.repository.UserRepository;
 import com.whiz.springbootmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.support.SimpleTriggerContext;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,9 +24,15 @@ public class UserService {
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object could not be found"));
     }
 
-    public User insert(User user){
-        return userRepository.insert(user);
+    public void insert(User user){
+        userRepository.insert(user);
     }
+
+    public void deleteById(String id){
+        findById(id);
+        userRepository.deleteById(id);
+    }
+
 
     public User fromDto(UserDTO userDTO){
         return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
