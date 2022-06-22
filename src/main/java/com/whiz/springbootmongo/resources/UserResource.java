f44eeda794dc6b8a77254f3f5a6c196e53984ba1
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-06-22 10:15:08 -0300

    Inserção de usuário com POST

--- src/main/java/com/whiz/springbootmongo/resources/UserResource.java
+++ src/main/java/com/whiz/springbootmongo/resources/UserResource.java
@@ -5,13 +5,11 @@ import com.whiz.springbootmongo.dto.UserDTO;
 import com.whiz.springbootmongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -37,6 +35,16 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+        User user = userService.fromDto(userDTO);
+        userService.insert(user);
+        //Cabecalho com a URI (endereco) do novo objeto criado "É uma boa pratica" - Nelio Alves
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("{/id}").buildAndExpand(userDTO.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/whiz/springbootmongo/service/UserService.java
+++ src/main/java/com/whiz/springbootmongo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.whiz.springbootmongo.service;
 
 import com.whiz.springbootmongo.domain.User;
+import com.whiz.springbootmongo.dto.UserDTO;
 import com.whiz.springbootmongo.repository.UserRepository;
 import com.whiz.springbootmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,13 @@ public class UserService {
     public User findById(String id){
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object could not be found"));
     }
+
+    public User insert(User user){
+        return userRepository.insert(user);
+    }
+
+    public User fromDto(UserDTO userDTO){
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+    }
+
 }
