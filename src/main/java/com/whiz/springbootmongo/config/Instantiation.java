62b44a9cbb74ff91d5e79bf59b2b6293655f0433
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-06-23 11:01:06 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/whiz/springbootmongo/config/Instantiation.java
+++ src/main/java/com/whiz/springbootmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.whiz.springbootmongo.config;
 
 import com.whiz.springbootmongo.domain.Post;
 import com.whiz.springbootmongo.domain.User;
+import com.whiz.springbootmongo.dto.AuthorDTO;
 import com.whiz.springbootmongo.repository.PostRepository;
 import com.whiz.springbootmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,11 +34,13 @@ public class Instantiation implements CommandLineRunner {
         User gui = new User(null, "Guilherme", "GuilhermeZ01@email");
         User carol = new User(null, "Carol", "Carol@Gmail.com");
         User nicole = new User(null, "Nicole", "NicoleMaria@Gmail");
-
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", carol);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia   ", "Acordei feliz hoje!", carol);
-
         userRepository.saveAll(List.of(gui, carol, nicole));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem",
+                "Vou viajar para São Paulo", new AuthorDTO(carol));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia   ",
+                "Acordei feliz hoje!", new AuthorDTO(carol));
+
         postRepository.saveAll(List.of(post1, post2));
     }
 }
